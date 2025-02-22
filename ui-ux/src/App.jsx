40e1b9a64017293dbd61a5881c61d56dd7bceb91
Hanz Chiziterem <hancarta23@gmail.com>
2025-02-22 14:28:28 +0100

    Implementing the basics of the todo-logic, also removed 'index.css' from 'main.jsx.'

--- ui-ux/src/App.jsx
+++ ui-ux/src/App.jsx
@@ -1,35 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [task, setTask] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(task)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <div className="container">
+      <div className="todo-header">
+        <h1>To-do List</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="todo-form">
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={task} placeholder="What tasks do you have in mind?" onChange={(e)=>setTask(e.target.value)}/>
+          <input type="submit" value="Add Task" />
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="todo-task-container">
+        
+      </div>
+     </div>
     </>
   )
 }
 
-export default App
+export default App;
